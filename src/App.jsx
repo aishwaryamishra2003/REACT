a8ff33e15f81a9032d6d7f044053a8936d1ce285
Aishwarya Mishra <144354501+aishwaryamishra2003@users.noreply.github.com>
2025-03-08 21:28:28 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
-  // Initialize the counter state
-  const [counter, setCounter] = useState(15);
-
-  const addValue = () => {
-    console.log("clicked", counter);
-    setCounter(counter + 1); // Update the counter state
+  const [count, setCount] = useState(0)
+  let myobj={
+    username:"aishwarya",
+    age:21
   }
-
-  const removeValue=()=>{
-    setCounter(counter-1);
-  } 
+  let newArr=[1,2,3]
   return (
     <>
-  <h1>Chai Aur react</h1>
-  <h2>Counter value:{counter}</h2>
-  <button
-  onClick={addValue}>Add Value:{counter}</button>
-  <br/>
-  <button
-  onClick={removeValue}>Remove value</button>
+     <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
+     <Card username="chai aur code" />
+     <Card username="aishwarya"/>
     </>
-  );
+  )
 }
 
-export default App;
-
+export default App
